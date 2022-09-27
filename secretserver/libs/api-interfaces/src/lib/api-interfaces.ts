d62ff31040b7d1b8b5ea6e37325b69253b8443ba
Francsics Gergely <frg2bp@bosch.com>
2022-09-27 20:19:54 +0200

    Add secret-api interfaces

--- secretserver/libs/api-interfaces/src/lib/api-interfaces.ts
+++ secretserver/libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,26 @@
 export interface Message {
   message: string;
 }
+
+export interface ReadSecretMetaDto {
+    hashedSecretText:	string
+    secretName:	string
+    remainingViews:	number
+    createdAt:	string
+    updatedAt:	string
+}
+
+export interface ReadSecretDto {
+  hashedSecretText:	string
+  secretName:	string
+  secretText:	string
+  remainingViews:	number
+  createdAt:	string
+  updatedAt:	string
+}
+
+export interface CreateSecretDto {
+  secretName:	string
+  secretText:	string
+  remainingViews:	number
+}
