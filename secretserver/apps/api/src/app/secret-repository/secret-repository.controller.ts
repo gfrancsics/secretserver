61c1f6f5292feb6780be688bb85f20254a753424
Francsics Gergely <frg2bp@bosch.com>
2022-09-28 20:50:54 +0200

    Controller and service basic calls

--- secretserver/apps/api/src/app/secret-repository/secret-repository.controller.ts
+++ secretserver/apps/api/src/app/secret-repository/secret-repository.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { SecretRepositoryService } from './secret-repository.service';
+import { CreateSecretDto, ReadSecretMetaDto } from '@secretserver/api-interfaces';
 
 @Controller('secret-repository')
-export class SecretRepositoryController {}
+export class SecretRepositoryController {
+    constructor(
+        private readonly secretService: SecretRepositoryService
+    ) {}
+
+    @Post('secrets')
+    async createSecretRequest(@Body() secretDto: CreateSecretDto): Promise<ReadSecretMetaDto> {
+        return this.secretService.createSecret(secretDto);
+    }
+
+    @Get('secrets')
+    async getAllSecretsRequest(): Promise<ReadSecretMetaDto[]> {
+        return this.secretService.getAllSecret();
+    }
+
+    @Get('secrets/:id')
+    async findSecretRequest(@Param() params): Promise<ReadSecretMetaDto> {
+        return this.secretService.findSecret(params.id);
+    }
+
+}
--- secretserver/apps/api/src/app/secret-repository/secret-repository.service.ts
+++ secretserver/apps/api/src/app/secret-repository/secret-repository.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateSecretDto, ReadSecretDto, ReadSecretMetaDto } from '@secretserver/api-interfaces';
 
 @Injectable()
-export class SecretRepositoryService {}
+export class SecretRepositoryService {
+
+    public async createSecret (secretDto : CreateSecretDto) : Promise<ReadSecretMetaDto> {
+        return new ReadSecretMetaDto("hash", "fake-secret", 1, "2022.01.01", "2022.02.02");
+    }
+
+    public async findSecret (hashedSecretText : string) : Promise<ReadSecretDto> {
+        return new ReadSecretDto("hash", "fake-secret", "fake", 1, "2022.01.01", "2022.02.02");
+    }
+
+    public async getAllSecret () : Promise<ReadSecretMetaDto[]> {
+        return [new ReadSecretMetaDto("hash", "fake-secret", 1, "2022.01.01", "2022.02.02")];
+    }
+}
