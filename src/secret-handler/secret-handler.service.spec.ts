fce01c1cc279c8762674d07481de1e3092cb42c1
Francsics Gergely <frg2bp@bosch.com>
2022-11-04 21:10:48 +0100

    SecretHnadlerService with mongoose mock

--- src/secret-handler/secret-handler.service.spec.ts
+++ src/secret-handler/secret-handler.service.spec.ts
@@ -4,10 +4,31 @@ import { CreateSecretDto } from '../dto/api-interfaces';
 import { mockModel } from '../../test/mock/mongoose.mock';
 import { SecretHandlerService } from '../secret-handler/secret-handler.service';
 import { Secret, SecretDocument } from '../secret-modell/secret-schema';
+import { Model } from 'mongoose';
 
 describe('SecretService', () => {
   let service: SecretHandlerService;
+  let model: Model<Secret>;
+
   const secretDto = new CreateSecretDto('', '', 1);
+  const secretArray = [
+    {
+      hashedSecretText: 'hash',
+      secretName: 'name1',
+      secretText: 'text1',
+      remainingViews: 1,
+      createdAt: '2022.11.04',
+      updatedAt: '2022.11.04',
+    },
+  ];
+  const secret = {
+    hashedSecretText: 'hash',
+    secretName: 'name1',
+    secretText: 'text1',
+    remainingViews: 1,
+    createdAt: '2022.11.04',
+    updatedAt: '2022.11.04',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,20 +40,47 @@ describe('SecretService', () => {
             new: jest.fn().mockResolvedValue(secretDto),
             constructor: jest.fn().mockResolvedValue(secretDto),
             find: jest.fn(),
+            findOne: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
             create: jest.fn().mockResolvedValue(secretDto),
-            exec: jest.fn(),
-            replaceOne: jest.fn(),
           },
         },
       ],
     }).compile();
-
     service = module.get<SecretHandlerService>(SecretHandlerService);
+    model = module.get<Model<Secret>>(getModelToken(Secret.name));
   });
 
-  it('should insert a new cat', async () => {
-    const newSecret = new CreateSecretDto('', '', 1);
-    const responseSecret = await service.createSecret(newSecret);
-    console.log(responseSecret);
+  describe('createSecret function', () => {
+    it('should insert a new secret', async () => {
+      jest.spyOn(model, 'findByIdAndUpdate').mockReturnValue({
+        exec: jest.fn().mockResolvedValueOnce(secretDto),
+      } as any);
+      const responseSecret = await service.createSecret(secretDto);
+      expect(secretDto.secretName).toEqual(responseSecret.secretName);
+    });
+  });
+
+  describe('getAllSecrets function', () => {
+    it('should return all the secrets', async () => {
+      jest.spyOn(model, 'find').mockReturnValue({
+        exec: jest.fn().mockResolvedValueOnce(secretArray),
+      } as any);
+      const responseSecret = await service.getAllSecrets();
+      expect(secretArray[0].secretName).toEqual(responseSecret[0].secretName);
+    });
+  });
+
+  describe('getSecret function', () => {
+    it('should find a secret by hashedtext', async () => {
+      jest.spyOn(model, 'findOne').mockReturnValue({
+        exec: jest.fn().mockResolvedValueOnce(secret),
+      } as any);
+      jest.spyOn(model, 'findByIdAndUpdate').mockReturnValue({
+        exec: jest.fn().mockResolvedValueOnce(secret),
+      } as any);
+      const responseSecret = await service.getSecret('hash');
+      expect(responseSecret.secretName).toEqual(secret.secretName);
+    });
   });
 });
